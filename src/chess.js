a14816d5783fc5fd45c8399c7867a5630a2acfaf
dhavalsinhzala <dhaval.zala@live.com>
2015-08-03 17:47:07 +0530

    resolved issues

--- src/chess.js
+++ src/chess.js
@@ -1,32 +1,28 @@
 function chess() {
-    var chessObj = {}, size = 8, startIndex = 0, pieces = {};
-    chessObj.pieceType = {
+    var obj = {}, size = 8, startIndex = 0, pieces = {};
+    obj.pieceType = {
         whiteQueen : 0,
         blackQueen : 1
     };
-    chessObj.initPiece = function(pieceType, row, column) {
+    obj.initPiece = function(pieceType, row, column) {
         pieces[pieceType] = null;
-        if (row && column && !isNaN(row) && !isNaN(column)) {
-            if (row < (size + startIndex) && column < (size + startIndex)
-                    && row >= startIndex && column >= startIndex) {
-                pieces[pieceType] = {
-                    row : row,
-                    column : column
-                };
-            }
+        if ((row && column && !isNaN(row) && !isNaN(column))
+                && (row < (size + startIndex) && column < (size + startIndex)
+                        && row >= startIndex && column >= startIndex)) {
+            
+            pieces[pieceType] = {
+                row : row,
+                column : column
+            };
+
         }
     };
-    chessObj.canAttack = function(onePiece, otherPiece) {
+    obj.canAttack = function(onePiece, otherPiece) {
         if (pieces[onePiece] && pieces[otherPiece]) {
             if (onePiece === this.pieceType.whiteQueen
                     || onePiece == this.pieceType.blackQueen) {
-                // row check - line equation x = c
-                if (pieces[onePiece].row === pieces[otherPiece].row) {
-                    return true;
-                }
-
-                // column check - line equation y = c
-                if (pieces[onePiece].column === pieces[otherPiece].column) {
+                // row check - line equation x = c and column check - line equation y = c
+                if (pieces[onePiece].row === pieces[otherPiece].row || pieces[onePiece].column === pieces[otherPiece].column) {
                     return true;
                 }
 
@@ -37,8 +33,13 @@ function chess() {
                     return true;
                 }
             }
+            
+            // other conditions will be here
+            else if(1){
+                
+            }
         }
         return false;
     };
-    return chessObj;
+    return obj;
 }
