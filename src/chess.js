ccf7e0f163e6caef3f8e3b3380d2b560023e2561
dhavalsinhzala <dhaval.zala@live.com>
2015-08-03 18:05:48 +0530

    indentation issues

--- src/chess.js
+++ src/chess.js
@@ -1,13 +1,13 @@
 function chess() {
     var obj = {}, size = 8, startIndex = 0, pieces = {};
     obj.pieceType = {
-        whiteQueen : 0,
-        blackQueen : 1
+            whiteQueen : 0,
+            blackQueen : 1
     };
     function isValid(number) {
         if (number) {
             return !isNaN(number) && number < (size + startIndex)
-                    && number >= startIndex;
+            && number >= startIndex;
         }
         return false;
     }
@@ -15,26 +15,27 @@ function chess() {
         pieces[pieceType] = null;
         if (isValid(row) && isValid(column)) {
             pieces[pieceType] = {
-                row : row,
-                column : column
+                    row : row,
+                    column : column
             };
 
         }
     };
     obj.canAttack = function(onePiece, otherPiece) {
-        if (pieces[onePiece] && pieces[otherPiece]) {
+        var piece =pieces[onePiece] , opponentPeice = pieces[otherPiece];
+        if ( piece&& opponentPeice) {
             if (onePiece === this.pieceType.whiteQueen
                     || onePiece === this.pieceType.blackQueen) {
                 // row check - line equation x = c and column check - line
                 // equation y = c
-                if (pieces[onePiece].row === pieces[otherPiece].row
-                        || pieces[onePiece].column === pieces[otherPiece].column) {
+                if (piece.row === opponentPeice.row
+                        || piece.column === opponentPeice.column) {
                     return true;
                 }
 
                 // diagonal check - line equation y = x + c or y = -x + c
-                var slope = (pieces[onePiece].row - pieces[otherPiece].row * 1.0)
-                        / (pieces[onePiece].column - pieces[otherPiece].column);
+                var slope = (piece.row - opponentPeice.row * 1.0)
+                / (piece.column - opponentPeice.column);
                 if (Math.abs(slope) === 1) {
                     return true;
                 }
