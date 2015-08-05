008dfa9d47a6f6b3f7dac3d44aa95d27589c8a3f
dhavalsinhzala <dhaval.zala@live.com>
2015-08-05 15:33:16 +0530

    resoved scope issues

--- src/chess.js
+++ src/chess.js
@@ -1,55 +1,63 @@
-function chess() {
-    var obj = {}, size = 8, startIndex = 0, pieces = {};
-    obj.pieceType = {
-            whiteQueen : 0,
-            blackQueen : 1
-    };
-    function isValid(number) {
-        if (number) {
-            return !isNaN(number) && number < (size + startIndex)
-            && number >= startIndex;
-        }
-        return false;
-    }
-    function canQueenAttack(queen, opponentPeice){
-        // row check - line equation x = c and column check - line
-        // equation y = c
-        if (queen.row === opponentPeice.row
-                || queen.column === opponentPeice.column) {
-            return true;
+var chess = (function () {
+    var obj = function(){
+        var size = 8, startIndex = 0, pieces = {};
+
+        this.pieceType = {
+                whiteQueen : 0,
+                blackQueen : 1
+        };
+
+        function isValid(number) {
+            if (number) {
+                return !isNaN(number) && number < (size + startIndex)
+                && number >= startIndex;
+            }
+            return false;
         }
 
-        // diagonal check - line equation y = x + c or y = -x + c
-        var slope = (queen.row - opponentPeice.row * 1.0)
-        / (queen.column - opponentPeice.column);
-        if (Math.abs(slope) === 1) {
-            return true;
-        }
-    }
-    obj.initPiece = function(pieceType, row, column) {
-        pieces[pieceType] = null;
-        if (isValid(row) && isValid(column)) {
-            pieces[pieceType] = {
-                    row : row,
-                    column : column
-            };
-
-        }
-    };
-    obj.canAttack = function(onePiece, otherPiece) {
-        var piece =pieces[onePiece] , opponentPeice = pieces[otherPiece];
-        if ( piece&& opponentPeice) {
-            if (onePiece === this.pieceType.whiteQueen
-                    || onePiece === this.pieceType.blackQueen) {
-                return canQueenAttack(piece, opponentPeice);
+        function canQueenAttack(queen, opponentPeice){
+            // row check - line equation x = c and column check - line
+            // equation y = c
+            if (queen.row === opponentPeice.row
+                    || queen.column === opponentPeice.column) {
+                return true;
             }
 
-            // other conditions will be here
-            else if (1) {
-                return false;
+            // diagonal check - line equation y = x + c or y = -x + c
+            var slope = (queen.row - opponentPeice.row * 1.0)
+            / (queen.column - opponentPeice.column);
+            if (Math.abs(slope) === 1) {
+                return true;
             }
         }
-        return false;
+
+        this.initPiece = function(pieceType, row, column) {
+            pieces[pieceType] = null;
+            if (isValid(row) && isValid(column)) {
+                pieces[pieceType] = {
+                        row : row,
+                        column : column
+                };
+
+            }
+        };
+
+        this.canAttack = function(onePiece, otherPiece) {
+            var piece =pieces[onePiece] , opponentPeice = pieces[otherPiece];
+            if ( piece&& opponentPeice) {
+                if (onePiece === this.pieceType.whiteQueen
+                        || onePiece === this.pieceType.blackQueen) {
+                    return canQueenAttack(piece, opponentPeice);
+                }
+
+                // other conditions will be here
+                else if (1) {
+                    return false;
+                }
+            }
+            return false;
+        };
     };
+
     return obj;
-}
+})();
